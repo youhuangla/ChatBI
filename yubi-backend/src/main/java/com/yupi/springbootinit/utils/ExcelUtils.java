c942d61ee41260abeb4a7360a021a6bd930c3745
youhuangla <youhuangla123@163.com>
2023-09-01 11:17:57 +0800

    Deduplication in excel

--- yubi-backend/src/main/java/com/yupi/springbootinit/utils/ExcelUtils.java
+++ yubi-backend/src/main/java/com/yupi/springbootinit/utils/ExcelUtils.java
@@ -1,15 +1,20 @@
 package com.yupi.springbootinit.utils;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.support.ExcelTypeEnum;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ExcelUtils {
     public static String excelToCsv(MultipartFile multipartFile) {
@@ -30,8 +35,14 @@ public class ExcelUtils {
         }
         // 转换为 csv
         // 读取表头
-        list.get(0);
-        Map<Integer, String> integerStringMap = list.get(0);
+        LinkedHashMap<Integer, String> headerMap = (LinkedHashMap) list.get(0);
+        List<String> headerList = headerMap.values().stream().filter(header -> ObjectUtils.isNotEmpty(header)).collect(Collectors.toList());
+        System.out.println(StringUtils.join(headerList, ","));
+        for (int i = 1; i < list.size(); i++) {
+            LinkedHashMap<Integer, String> dataMap = (LinkedHashMap) list.get(i);
+            List<String> dataList = dataMap.values().stream().filter(header -> ObjectUtils.isNotEmpty(header)).collect(Collectors.toList());
+            System.out.println(StringUtils.join(dataList, ","));
+        }
         System.out.println(list);
         return "";
     }
