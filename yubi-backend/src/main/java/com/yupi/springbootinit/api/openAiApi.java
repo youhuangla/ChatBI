eb8ba63f2b4f77b3580baba8d96cef6b28b2a0e1
youhuangla <youhuangla123@163.com>
2023-09-07 10:03:36 +0800

    Imply a GPT demo using api2d

---
+++ yubi-backend/src/main/java/com/yupi/springbootinit/api/openAiApi.java
@@ -0,0 +1,67 @@
+package com.yupi.springbootinit.api;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.json.JSONUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class openAiApi {
+    public static void main(String[] args) {
+        String url = "https://oa.api2d.net/v1/chat/completions";
+
+        // 创建请求体
+        Map<String, Object> bodyMap = new HashMap<>();
+        bodyMap.put("model", "gpt-3.5-turbo");
+        Map<String, String> message = new HashMap<>();
+        message.put("role", "user");
+        message.put("content", "讲个笑话");
+        bodyMap.put("messages", new Map[] {message});
+        bodyMap.put("safe_mode", false);
+
+        String jsonBody = JSONUtil.toJsonStr(bodyMap);
+
+        // 发送请求
+        HttpResponse response = HttpRequest.post(url)
+                .header("Authorization", "Bearer fk......")  // 请替换为您的真实token
+                .header("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+                .header("Content-Type", "application/json")
+                .body(jsonBody)
+                .timeout(0)  // 设置超时时间为无限
+                .execute();
+
+        if(response.isOk()) {
+            System.out.println(response.body());
+        } else {
+            System.out.println("请求失败，状态码：" + response.getStatus());
+        }
+    }
+}
+
+/*
+package com.yupi.springbootinit.api;
+
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class openAiApi {
+    public static void main(String[] args) {
+        String url = "https://api.openai.com/v1/chat/completions";
+        Map<String, Object> hashMap = new HashMap<>();
+        hashMap.put("message", "用户的消息，请帮我分析");
+        String json = JSONUtil.toJsonStr(hashMap);
+        String result = HttpRequest.post(url)
+                .header("Authorization", "Bearer sk-......")
+                .body(json)
+                .execute()
+                .body();
+        System.out.println(result);
+        return ;
+    }
+}
+*/
--- yubi-backend/src/main/java/com/yupi/springbootinit/controller/ChartController.java
+++ yubi-backend/src/main/java/com/yupi/springbootinit/controller/ChartController.java
@@ -233,7 +233,16 @@ public class ChartController {
         ThrowUtils.throwIf(StringUtils.isBlank(goal), ErrorCode.PARAMS_ERROR, "目标为空");
         ThrowUtils.throwIf(StringUtils.isNotBlank(name) && name.length() > 100, ErrorCode.PARAMS_ERROR, "目标过长");
 
-
+        final String prompt = "你是一个数据分析师和前端开发专家，接下来我会按照以下固定格式给你提供内容：\n" +
+                "分析需求：\n" +
+                "{数据分析的需求或者目标}\n" +
+                "原始数据：\n" +
+                "{csv格式的原始数据，用，作为分隔符}\n" +
+                "请根据这两部分内容，按照以下指定格式生成内容（此外不要输出任何多余的开头、结尾、注释）\n" +
+                "【【【【【\n" +
+                "{前端Echarts V5的option配置对象js代码，合理地将数据进行可视化，不要生成任何多余的内容，比如注释}\n" +
+                "【【【【I\n" +
+                "{明确的数据分析结论、越详细越好，不要生成多余的注释}";
         // 用户输入
         StringBuilder userInput = new StringBuilder();
         userInput.append("你是一个数据分析师，接下来我会给你我的分析目标和原始数据，请告诉我分析结论。").append("\n");
