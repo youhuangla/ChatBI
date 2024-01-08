90e499ca3e3b1dfc0f3985b051b4f0d35fd56ac9
youhuangla <youhuangla123@163.com>
2024-01-08 11:04:59 +0800

    Increase the margin of search and chart on MyChart page using css

--- yubi-frontend/src/pages/MyChart/index.tsx
+++ yubi-frontend/src/pages/MyChart/index.tsx
@@ -65,6 +65,7 @@ const MyChartPage: React.FC = () => {
           }}
         />
       </div>
+      <div className="margin-16" />
       <List
         grid={{ gutter: 16, xs: 1, sm: 1, md: 1, lg: 2, xl: 2, xxl: 2 }}
         pagination={{
