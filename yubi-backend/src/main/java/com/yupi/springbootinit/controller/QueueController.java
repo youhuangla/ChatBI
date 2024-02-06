f9d17f8d748d81f5a271c9889ab0fafc1ef880e4
youhuangla <youhuangla123@163.com>
2024-02-06 09:21:02 +0800

    Thread pool TEST succeed

---
+++ yubi-backend/src/main/java/com/yupi/springbootinit/controller/QueueController.java
@@ -0,0 +1,80 @@
+package com.yupi.springbootinit.controller;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.google.gson.Gson;
+import com.yupi.springbootinit.annotation.AuthCheck;
+import com.yupi.springbootinit.common.BaseResponse;
+import com.yupi.springbootinit.common.DeleteRequest;
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.common.ResultUtils;
+import com.yupi.springbootinit.constant.CommonConstant;
+import com.yupi.springbootinit.constant.UserConstant;
+import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.springbootinit.exception.ThrowUtils;
+import com.yupi.springbootinit.manager.AiManager;
+import com.yupi.springbootinit.manager.RedisLimiterManager;
+import com.yupi.springbootinit.model.dto.chart.*;
+import com.yupi.springbootinit.model.entity.Chart;
+import com.yupi.springbootinit.model.entity.User;
+import com.yupi.springbootinit.model.vo.BiResponse;
+import com.yupi.springbootinit.service.ChartService;
+import com.yupi.springbootinit.service.UserService;
+import com.yupi.springbootinit.utils.ExcelUtils;
+import com.yupi.springbootinit.utils.SqlUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 帖子接口
+ *
+ * 
+ */
+@RestController
+@RequestMapping("/queue")
+@Slf4j
+public class QueueController {
+    @Resource
+    private ThreadPoolExecutor threadPoolExecutor;
+    @GetMapping("/add")
+    public void add(String name) {
+        System.out.println("任务开始：" + name); // 记录任务被提交给线程池的时间点
+        CompletableFuture.runAsync(() -> {
+            // System.out.println("任务执行中：" + name + "，执行人：" + Thread.currentThread().getName());
+            log.info("任务执行中：" + name + "，执行人：" + Thread.currentThread().getName());
+            try {
+                Thread.sleep(600000);// 10min
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
+        }, threadPoolExecutor); // 使用线程池，若不使用线程池则会使用ForkJoinPool.commonPool()线程池，默认线程数为CPU核心数。可以点进runAsync方法查看
+    };
+    @GetMapping("/get")
+    public String get() {
+        Map<String, Object> map = new HashMap<>();
+        int size = threadPoolExecutor.getQueue().size();
+        map.put("队列长度", size);
+        long taskCount = threadPoolExecutor.getTaskCount();
+        map.put("任务总数", taskCount);
+        long completedTaskCount = threadPoolExecutor.getCompletedTaskCount();
+        map.put("已完成任务数", completedTaskCount);
+        int activeCount = threadPoolExecutor.getActiveCount();
+        map.put("正在工作的线程数", activeCount);
+        return JSONUtil.toJsonStr(map);
+    }
+}
